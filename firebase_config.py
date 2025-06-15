f723b36cef2d5117692ab829d8dba759d9932064
Sandra R. Zegarrundo <64284099+SandraRojasZ@users.noreply.github.com>
2025-06-15 19:50:55 -0300

    Update firebase_config.py
    exemplo da credentials

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 
 # Inicializa o Firebase
 def init_firebase():
-    cred = credentials.Certificate("C:\\Users\\Sandra\\multiplataforma\\flask-server\\firebase-auth.json")
+    cred = credentials.Certificate("C:\\Users\\Nome\\multiplataforma\\pasta\\chave.json")
     firebase_admin.initialize_app(cred)
     return firestore.client()
 
